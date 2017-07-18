d29b3233f1984c149cb6316e454f8f15de9bae3e
Local Admin <Local Admin>
2017-07-18 14:23:04 +0100

    Changed docker-compose to attempt to access providerstates URL. Added this in as a URL in API code

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^swagger', schema_view)
+    url(r'^swagger', schema_view),
+    url(r'providerstates', views.provider_states)
 ]
